b91fdc2608ea155d1d6e79a3477ce5053f9e6d60
Kevin Molid <kevinmolid@gmail.com>
2024-02-23 23:11:00 +0100

    Removed Strict Mode for testing, decoding text

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import {decode} from 'html-entities'
 
-decode('&lt; &gt; &quot; &apos; &amp; &#169; &#8710;')
-
 export default function Question(props) {
+
     return (
         <div className="question">
-            <h2 className="question--question">{props.question}</h2>
+            <h2 className="question--question">{decode(props.question)}</h2>
             <ul className="question--answers">
-                <li className="answer">{props.answer1}</li>
-                <li className="answer">{props.answer2}</li>
-                <li className="answer">{props.answer3}</li>
-                <li className="answer">{props.answer4}</li>
+                <li className="answer">{decode(props.answer1)}</li>
+                <li className="answer">{decode(props.answer2)}</li>
+                <li className="answer">{decode(props.answer3)}</li>
+                <li className="answer">{decode(props.answer4)}</li>
             </ul>
         </div>
     )
