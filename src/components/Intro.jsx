55eccfa62957da39951d55e8d603b163e23cfc22
Kevin Molid <kevinmolid@gmail.com>
2024-02-23 22:30:00 +0100

    Imported logo

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '/src/assets/logo.png'
 
 export default function Intro(props) {
     return (
         <>
-            <img className="intro--logo-img" src="src\assets\logo.png"/>
+            <img className="intro--logo-img" src={logo}/>
             <h1 className="intro--logo-txt">Quizzly Bear</h1>
             <p className="intro--txt">Dive into the den of knowledge</p>
             <button onClick={props.startGame} className="intro--btn">Play now!</button>
