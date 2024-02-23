1509e685f97043880438832266225672b56af436
Kevin Molid <kevinmolid@gmail.com>
2024-02-23 21:23:22 +0100

    Hooked up start button

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import Intro from './components/Intro'
 import Quiz from './components/Quiz'
 
 function App() {
-  const [playing, setPlaying] = useState(true)
+  const [playing, setPlaying] = useState(false)
+
+  const [questions, setQuestions] = useState([])
+
+  function startGame() {
+    setPlaying(prevState => !prevState)
+  }
 
   return (
     <>
-      {!playing && <Intro />}
-      <Quiz />
+      {!playing && <Intro startGame={startGame}/>}
+      {playing && <Quiz />}
     </>
   )
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Intro() {
+export default function Intro(props) {
     return (
         <>
             <img className="intro--logo-img" src="src\assets\logo.png"/>
             <h1 className="intro--logo-txt">Quizzly Bear</h1>
             <p className="intro--txt">Dive into the den of knowledge</p>
-            <button className="intro--btn">Play now!</button>
+            <button onClick={props.startGame} className="intro--btn">Play now!</button>
         </>
     )
 }
